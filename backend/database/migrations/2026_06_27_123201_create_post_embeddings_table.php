ee8d3f9685cde0323d35b7dec148ac06ffc9f9a7
rajath <rajathkumar120@gmail.com>
2026-06-28 11:14:13 +0530

    hotfix: migration change in post_embedding

--- backend/database/migrations/2026_06_27_123201_create_post_embeddings_table.php
+++ backend/database/migrations/2026_06_27_123201_create_post_embeddings_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -12,19 +13,22 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('post_embeddings', function (Blueprint $table) {
-            $table->foreignId('post_id')
-                ->primary()
-                ->constrained('posts')
-                ->cascadeOnDelete();
 
-            $table->text('embedding');
+            $table->foreignId('post_id')
+                ->constrained()
+                ->cascadeOnDelete();
 
             $table->string('model_name');
 
             $table->string('model_version');
-            
+
             $table->timestamp('created_at');
         });
+
+        DB::statement("
+        ALTER TABLE post_embeddings
+        ADD COLUMN embedding vector(384)
+    ");
     }
 
     /**
